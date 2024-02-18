8677b5c83f194d2583080575c9806d0733e6d5fe
saul <solleedata@gmail.com>
2024-02-18 11:19:47 +0900

    refactor: ThirdwebProvider in a separate component file

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 "use client"
 
+import { ThirdwebProvider } from "./components/ThirdwebProvider"
+import {
+ metamaskWallet,
+ coinbaseWallet,
+ walletConnect,
+} from "@thirdweb-dev/react"
 import { ConnectWallet } from "@thirdweb-dev/react"
 import { MediaRenderer } from "@thirdweb-dev/react"
 
 export default function Home() {
  return (
-  <div>
+  <ThirdwebProvider
+   supportedWallets={[
+    metamaskWallet({
+     recommended: true,
+    }),
+    coinbaseWallet(),
+    walletConnect(),
+   ]}
+   clientId={process.env.THIRDWEB_API_KEY}>
    <ConnectWallet />
 
    <MediaRenderer src="ipfs://QmV4HC9fNrPJQeYpbW55NLLuSBMyzE11zS1L4HmL6Lbk7X" />
-  </div>
+  </ThirdwebProvider>
  )
 }
